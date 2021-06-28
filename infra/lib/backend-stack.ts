2ab583bd82b248a0deca2fe005b8907e171c343d
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-06-28 07:19:22 +0100

    basic apigw and lambda for the diceRoll webservice

--- infra/lib/backend-stack.ts
+++ infra/lib/backend-stack.ts
@@ -1,10 +1,11 @@
-import {Construct, Stack, StackProps, CfnOutput} from "@aws-cdk/core";
+import {CfnOutput, Construct, Stack, StackProps} from "@aws-cdk/core";
 import * as lambda from '@aws-cdk/aws-lambda';
-import path = require('path');
 import * as apigw from '@aws-cdk/aws-apigateway'
+import path = require('path');
 
 export class BackendStack extends Stack {
     urlOutput: CfnOutput;
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
@@ -21,11 +22,94 @@ export class BackendStack extends Stack {
         })
 
         // apigateway
-        const api = new apigw.LambdaRestApi(this, 'DiceGateway', {
+        const api = new apigw.RestApi(this, 'DiceGateway', {
             description: 'Endpoint for a simple Lambda-powered web service',
-            handler: alias,
         });
 
-        this.urlOutput = new CfnOutput(this, 'url', { value: api.url });
+        const diceRoll = api.root.addResource('diceRoll')
+
+        // We define the JSON Schema for the transformed valid response
+        const responseModel = api.addModel('ResponseModel', {
+            contentType: 'application/json',
+            modelName: 'ResponseModel',
+            schema: {
+                schema: apigw.JsonSchemaVersion.DRAFT4,
+                title: 'pollResponse',
+                type: apigw.JsonSchemaType.OBJECT,
+                properties: {
+                    state: {type: apigw.JsonSchemaType.STRING},
+                    greeting: {type: apigw.JsonSchemaType.STRING}
+                }
+            }
+        });
+
+// We define the JSON Schema for the transformed error response
+        const errorResponseModel = api.addModel('ErrorResponseModel', {
+            contentType: 'application/json',
+            modelName: 'ErrorResponseModel',
+            schema: {
+                schema: apigw.JsonSchemaVersion.DRAFT4,
+                title: 'errorResponse',
+                type: apigw.JsonSchemaType.OBJECT,
+                properties: {
+                    state: {type: apigw.JsonSchemaType.STRING},
+                    message: {type: apigw.JsonSchemaType.STRING}
+                }
+            }
+        });
+
+        const postDiceRollIntegration = new apigw.LambdaIntegration(handler, {
+            proxy: false,
+            allowTestInvoke: true,
+            passthroughBehavior: apigw.PassthroughBehavior.WHEN_NO_MATCH,
+            integrationResponses: [
+                {
+                    statusCode: "200",
+                    responseParameters: {
+                        'method.response.header.Content-Type': "'application/json'",
+                        'method.response.header.Access-Control-Allow-Origin': "'*'",
+                        'method.response.header.Access-Control-Allow-Credentials': "'true'"
+                    },
+
+                },
+                {
+                    selectionPattern: '(\n|.)+',
+                    statusCode: "400",
+                    responseParameters: {
+                        'method.response.header.Content-Type': "'application/json'",
+                        'method.response.header.Access-Control-Allow-Origin': "'*'",
+                        'method.response.header.Access-Control-Allow-Credentials': "'true'"
+                    }
+                }
+            ]
+        })
+        diceRoll.addMethod('POST', postDiceRollIntegration,
+            {
+                methodResponses: [
+                    {
+                        // Successful response from the integration
+                        statusCode: '200',
+                        // Define what parameters are allowed or not
+                        responseParameters: {
+                            'method.response.header.Content-Type': true,
+                            'method.response.header.Access-Control-Allow-Origin': true,
+                            'method.response.header.Access-Control-Allow-Credentials': true
+                        },
+                    },
+                    {
+                        // Same thing for the error responses
+                        statusCode: '400',
+                        responseParameters: {
+                            'method.response.header.Content-Type': true,
+                            'method.response.header.Access-Control-Allow-Origin': true,
+                            'method.response.header.Access-Control-Allow-Credentials': true
+                        },
+                    }
+                ]
+            })
+
+
+        this.urlOutput = new CfnOutput(this, 'url', {value: api.urlForPath("/")});
+
     }
 }
